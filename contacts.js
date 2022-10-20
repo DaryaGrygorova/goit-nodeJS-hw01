3a181126710fd74332505e4d486d7717b95f6916
DaryaGrygorova <grygorova.darya@gmail.com>
2022-10-20 14:32:47 +0300

    Refactoring to async/await

--- contacts.js
+++ contacts.js
@@ -5,79 +5,62 @@ const colors = require("colors");
 const contactPath = path.resolve("./db/contacts.json");
 const textFormat = "utf8";
 
-function listContacts() {
-  fs.readFile(contactPath, textFormat)
-    .then((response) => {
-      console.table(JSON.parse(response));
-    })
-    .catch((err) => console.log(err));
-}
+async function listContacts() {
+  try {
+    const response = await fs.readFile(contactPath, textFormat);
+    const data = JSON.parse(response);
+    console.table(data);
+  }
+  catch (err) { console.log(err) };
+};
 
-function getContactById(contactId) {
-  fs.readFile(contactPath, textFormat)
-    .then((response) =>
-      console.table(
-        JSON.parse(response).filter((item) => item.id === contactId.toString())
-      )
+async function getContactById(contactId) {
+  try {
+    const response = await fs.readFile(contactPath, textFormat);
+    const data = JSON.parse(response);
+    console.table(
+      data.filter((item) => item.id === contactId.toString())
     )
-    .catch((err) => console.log(err));
-}
+  }
+  catch (err) { console.log(err) };
+};
 
-function removeContact(contactId) {
-  fs.readFile(contactPath, textFormat)
-    .then((response) => {
-      const data = JSON.parse(response);
-      if (
-        !contactId ||
-        contactId >= response.length ||
-        !data.find((item) => item.id === contactId.toString())
-      ) {
-        console.log(`Failed to delete contact with ID '${contactId}'!`.red);
-      } else {
-        const newData = data.filter((item) => item.id !== contactId.toString());
+async function removeContact(contactId) {
+  try {
+    const response = await fs.readFile(contactPath, textFormat);
+    const data = JSON.parse(response);
 
-        fs.writeFile(contactPath, JSON.stringify(newData), textFormat)
-          .then(
-            fs
-              .readFile(contactPath, textFormat)
-              .then((response) => {
-                console.table(JSON.parse(response));
-              })
-              .catch((err) => console.log(err))
-          )
-          .catch((err) => console.log(err));
-      }
-    })
-    .catch((err) => console.log(err));
-}
+    if (!data.find((item) => item.id === contactId.toString())) {
+      console.log(`Failed to delete contact with ID '${contactId}'!`.red);
+    } else {
+      const newData = data.filter((item) => item.id !== contactId.toString());
+
+      await fs.writeFile(contactPath, JSON.stringify(newData), textFormat)
+      listContacts()
+    }
+  }
+  catch (err) { console.log(err) };
+};
 
 async function addContact(name, email, phone) {
-  fs.readFile(contactPath, textFormat)
-    .then((response) => {
-      const data = JSON.parse(response);
+  try {
+    const response = await fs.readFile(contactPath, textFormat);
+    const data = JSON.parse(response);
 
-      if (data.find((item) => item.name === name)) {
-        console.log(`A contact with name '${name}' is already exists!`.red);
-      } else {
-        const id = (+data[data.length - 1].id + 1).toString();
-        const newContent = { id, name, email, phone };
+    if (data.find((item) => item.name === name)) {
+      console.log(`A contact with name '${name}' is already exists!`.red);
+    } else {
+      const id = (+data[data.length - 1].id + 1).toString();
+      const newContent = { id, name, email, phone };
+  
+      data.push(newContent);
 
-        data.push(newContent);
-
-        fs.writeFile(contactPath, JSON.stringify(data), textFormat)
-          .then(
-            fs
-              .readFile(contactPath, textFormat)
-              .then((response) => {
-                console.table(JSON.parse(response));
-              })
-              .catch((err) => console.log(err))
-          )
-          .catch((err) => console.log(err));
-      }
-    })
-    .catch((err) => console.log(err));
-}
+      await fs.writeFile(contactPath, JSON.stringify(data), textFormat)
+      listContacts()
+    }
+  }
+  catch (err) { console.log(err) };
+};
 
 module.exports = {
   listContacts,
